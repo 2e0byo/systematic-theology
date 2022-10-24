b029f5d0e2b017fe3ae60b04b40833fee8c5069b
John Maximilian <2e0byo@gmail.com>
2022-10-24 17:06:29 +0100

    feat: week 4.

--- scripts/build.py
+++ scripts/build.py
@@ -19,3 +19,9 @@ for masterf in list(Path("slides").glob("*.html")):  # not sure if it updates
     clientf = OUTDIR / masterf.with_name(masterf.stem + "-client.html").name
     clientf.write_text(as_client(masterf.read_text()))
     copy(masterf, OUTDIR / masterf.with_name(masterf.stem + "-master.html").name)
+
+for imgf in Path("slides").glob("*.png"):
+    copy(imgf, OUTDIR / imgf.name)
+
+for imgf in Path("slides").glob("*.jpg"):
+    copy(imgf, OUTDIR / imgf.name)
